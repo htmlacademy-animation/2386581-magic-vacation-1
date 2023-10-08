e826a247cba7e4dc6498a3ae1170b30b0896022c
annazubareva14 <annazubareva14@yandex.ru>
2023-10-08 15:50:43 +0300

    animate slider, fix prizes screen

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -3,7 +3,27 @@ import Swiper from "swiper";
 export default () => {
   let storySlider;
   let sliderContainer = document.getElementById(`story`);
+  const pageHeader = document.querySelector(`.page-header`);
+  const headerClasses = [`page-header--water`, `page-header--ice`, `page-header--light`];
   sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
+
+  document.body.addEventListener(`screenChanged`, (e) => {
+    changeHeaderClass(e.detail.screenName);
+  });
+
+  const changeHeaderClass = (screen) => {
+    pageHeader.classList.remove(...headerClasses);
+    if (screen && screen !== `story`) {
+      return;
+    }
+    if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
+      pageHeader.classList.add(headerClasses[2]);
+    } else if (storySlider.activeIndex === 2 || storySlider.activeIndex === 3) {
+      pageHeader.classList.add(headerClasses[0]);
+    } else if (storySlider.activeIndex === 4 || storySlider.activeIndex === 5) {
+      pageHeader.classList.add(headerClasses[1]);
+    }
+  };
 
   const setSlider = function () {
     if (((window.innerWidth / window.innerHeight) < 1) || window.innerWidth < 769) {
@@ -26,6 +46,7 @@ export default () => {
             } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
             }
+            changeHeaderClass();
           },
           resize: () => {
             storySlider.update();
@@ -60,6 +81,7 @@ export default () => {
             } else if (storySlider.activeIndex === 6) {
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg")`;
             }
+            changeHeaderClass();
           },
           resize: () => {
             storySlider.update();
